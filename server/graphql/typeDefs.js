6ce1efe658592fce5f25a4ef644391244e1e3713
SanzharNussipbek <51924921+SanzharNussipbek@users.noreply.github.com>
2021-11-27 18:36:27 +0000

    fix: type definitions and add queries and mutations

--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -32,12 +32,14 @@ module.exports = gql`
     id: ID!
     seatNumber: Int!
     hallId: ID!
+    createdAt: String!
   }
   type Hall {
     id: ID!
     name: String!
     type: String!
     totalSeats: Int!
+    createdAt: String!
   }
   type Ticket {
     id: ID!
@@ -46,7 +48,8 @@ module.exports = gql`
     price: String!
     status: String!
     timestamp: String!
-    promocode: String!
+    promocode: String
+    createdAt: String!
   }
   type Session {
     id: ID!
@@ -55,6 +58,7 @@ module.exports = gql`
     date: String!
     startTime: String!
     endTime: String!
+    createdAt: String!
   }
   type SessionSeat {
     id: ID!
@@ -63,7 +67,8 @@ module.exports = gql`
     ticketId: String!
     type: String!
     status: String!
-    price: Int!
+    price: String!
+    createdAt: String!
   }
   input RegisterInput {
     email: String!
@@ -79,26 +84,98 @@ module.exports = gql`
     password: String
     phone: String
   }
+  input CreateMovieInput {
+    name: String!
+    description: String!
+    duration: String!
+    language: String!
+    releaseDate: String!
+    country: String!
+    genre: String!
+    director: String!
+    cast: String!
+    rating: String!
+  }
+  input CreateHallInput {
+    name: String!
+    type: String!
+    totalSeats: Int!
+  }
+  input CreateSeatInput {
+    seatNumber: Int!
+    hallId: ID!
+  }
+  input CreateSessionInput {
+    movieId: ID!
+    hallId: ID!
+    date: String!
+    startTime: String!
+    endTime: String!
+  }
+  input CreateSessionSeatInput {
+    seatId: ID!
+    sessionId: String!
+    ticketId: String!
+    type: String!
+    status: String!
+    price: String!
+  }
+  input CreateTicketInput {
+    sessionId: ID!
+    userId: ID!
+    price: String!
+    status: String!
+    timestamp: String!
+    promocode: String!
+  }
+  input CreateMovieInput {
+    name: String!
+    description: String!
+    duration: String!
+    language: String!
+    releaseDate: String!
+    country: String!
+    genre: String!
+    director: String!
+    cast: String!
+    imgUrl: String!
+    rating: String!
+  }
   type Query {
     getMovies: [Movie]
-    getMovie(movieId: ID!): Movie
+    getMovie(id: ID!): Movie
+    getHalls: [Hall]
+    getHall(id: ID!): Hall
+    getSeats: [Seat]
+    getSeat(id: ID!): Seat
+    getSessions: [Session]
+    getSession(id: ID!): Session
+    getSessionSeats: [SessionSeat]
+    getSessionSeat(id: ID!): SessionSeat
+    getTickets: [Ticket]
+    getTicket(id: ID!): Ticket
   }
   type Mutation {
     register(registerInput: RegisterInput): User!
     login(email: String!, password: String!): User!
     updateUser(updateUserInput: UpdateUserInput): User!
-    createMovie(
-      name: String!
-      description: String!
-      duration: String!
-      language: String!
-      releaseDate: String!
-      country: String!
-      genre: String!
-      director: String!
-      cast: String!
-      rating: String!
-    ): Movie!
-    deleteMovie(movieId: ID!): String!
+
+    createMovie(data: CreateMovieInput): Movie!
+    deleteMovie(id: ID!): String!
+
+    createHall(data: CreateHallInput): Hall!
+    deleteHall(id: ID!): String!
+
+    createSeat(data: CreateSeatInput): Seat!
+    deleteSeat(id: ID!): String!
+
+    createSession(data: CreateSessionInput): Session!
+    deleteSession(id: ID!): String!
+
+    createSessionSeat(data: CreateSessionSeatInput): SessionSeat!
+    deleteSessionSeat(id: ID!): String!
+
+    createTicket(data: CreateTicketInput): Ticket!
+    deleteTicket(id: ID!): String!
   }
 `;
